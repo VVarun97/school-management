997d48867c7655ebcf967eda2566e263e900ec1b
VVarun97 <112501919+VVarun97@users.noreply.github.com>
2026-04-24 13:13:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -67,4 +67,6 @@ app.get('/listSchools', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
+  console.log("Server running");
+});
